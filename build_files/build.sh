ba7a9329f7acf543d4d99ad2c3f6d0262a66a69e
shawly <shawlyde@gmail.com>
2026-08-24 14:20:01 +0200

    feat(build): install plasma-bigscreen
    Adds the first dnf5 install line to build_files/build.sh with
    plasma-bigscreen on it. It brings in plasma-nano,
    plasma-bigscreen-wayland, libcec and platform, which gives the image
    /usr/bin/plasma-bigscreen-wayland and
    /usr/share/wayland-sessions/plasma-bigscreen-wayland.desktop.

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,3 +8,8 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# plasma-bigscreen pulls in plasma-nano, the Bigscreen KCMs and
+# plasma-bigscreen-inputhandler along with the wayland session file.
+dnf5 install -y \
+    plasma-bigscreen
